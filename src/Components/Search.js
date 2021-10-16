3927345b48c0a89c6715eef7f3dd47f674d4e332
Geraldoeze <egerald344@gmail.com>
2021-10-16 14:10:09 +0100

    worked on summary

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,13 +6,17 @@ const SearchBar = () => {
     return(
         <div className="Search">
             <h1>Location </h1>
-            <div> 
+            <div className="first"> 
                 <h3>Pick-up</h3>
               <input className="pick" />
             </div>
-            <div>
+            <div className="second">
                 <h3>Drop-off</h3>
             <input className="drop-off" />
+            </div>
+            <div className="click">
+                <button className="submit"> Submit</button>
+                <button className="submit" > Reset </button>
             </div>
         </div>
     )
